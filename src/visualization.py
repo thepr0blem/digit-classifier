2ead837a9f40e06ae28a8514c0b1674932e2481a
Daniel Gawlowski <gawlowski.daniel@gmail.com>
2018-11-04 01:41:49 +0100

    -

--- src/visualization.py
+++ src/visualization.py
@@ -1,7 +1,7 @@
-import numpy as np
 from matplotlib import pyplot as plt
-import random as rd
 import seaborn as sns
+import numpy as np
+import random as rd
 
 
 def plot_samples(X, y, labels):
@@ -56,3 +56,21 @@ def plot_conf_mat(conf_mat, label_list, normalize=False):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+
+
+def display_errors(X, y_true, y_pred):
+    """This function shows 9 wrongly classified images (randomly chosen)
+    with their predicted and real labels """
+
+    errors = (y_true - y_pred != 0).reshape(len(y_pred), )
+
+    X_errors = X[errors]
+    y_true_errors = y_true[errors]
+    y_pred_errors = y_pred[errors]
+
+    fig, ax = plt.subplots(3, 3, sharex=True, sharey=True)
+    for row in range(3):
+        for col in range(3):
+            n = rd.randint(0, len(X_errors))
+            ax[row, col].imshow(X_errors[n])
+            ax[row, col].set_title("Predicted label :{}\nTrue label :{}".format(y_pred_errors[n], y_true_errors[n]))
